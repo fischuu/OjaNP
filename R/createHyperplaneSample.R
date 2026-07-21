9a2b73d3b6040840705c5c92ee76391bb7faf613
Daniel Fischer <git@danielfischer.name>
2026-07-21 13:14:56 +0300

    Cran submission issues fixed

--- R/createHyperplaneSample.R
+++ R/createHyperplaneSample.R
@@ -1,26 +1,54 @@
-`createHyperplaneSample` <- function(N,pN,n,k){
-    FailureMessage <- "Can't allocate enough memory: matrix 'X' is too large."
-    op <- options(warn=-1)  
-    on.exit(options(op))
-    if (pN < N){  
-        Sample <- try(sample(N,size=pN,replace=FALSE),silent=TRUE) # draws pN distinct numbers out of 1,...,N
-        if (inherits(Sample,"try-error")){
-            Sample <- try(floor((N*runif(n=pN))+0.5),silent=TRUE)
-        } 
-        if (inherits(Sample,"try-error")){
-            stop(FailureMessage)         
-        }
-        else{
-            CombinationsSample <- try(t(combn(x=n,m=k)[,Sample,drop=FALSE]),silent=TRUE)
-            if (inherits(CombinationsSample,"try-error")){
-                CombinationsSample <- try(combinationsSample(n=n,k=k,vec=Sample),silent=TRUE)
-            }
-        }
-    }else{
-        CombinationsSample <- try(t(combn(x=n,m=k)),silent=TRUE)          
+`createHyperplaneSample` <- function(N, pN, n, k) {
+  
+  FailureMessage <- "Can't allocate enough memory: matrix 'X' is too large."
+  
+  if (pN < N) {
+    
+    # Draw pN distinct numbers from 1,...,N
+    Sample <- suppressWarnings(
+      try(sample(N, size = pN, replace = FALSE), silent = TRUE)
+    )
+    
+    if (inherits(Sample, "try-error")) {
+      Sample <- suppressWarnings(
+        try(floor((N * runif(n = pN)) + 0.5), silent = TRUE)
+      )
     }
-    if (inherits(CombinationsSample,"try-error")){
-        stop(FailureMessage)
+    
+    if (inherits(Sample, "try-error")) {
+      stop(FailureMessage)
     }
-    return(CombinationsSample)
+    
+    CombinationsSample <- suppressWarnings(
+      try(
+        t(combn(x = n, m = k)[, Sample, drop = FALSE]),
+        silent = TRUE
+      )
+    )
+    
+    if (inherits(CombinationsSample, "try-error")) {
+      CombinationsSample <- suppressWarnings(
+        try(
+          combinationsSample(n = n, k = k, vec = Sample),
+          silent = TRUE
+        )
+      )
+    }
+    
+  } else {
+    
+    CombinationsSample <- suppressWarnings(
+      try(
+        t(combn(x = n, m = k)),
+        silent = TRUE
+      )
+    )
+    
+  }
+  
+  if (inherits(CombinationsSample, "try-error")) {
+    stop(FailureMessage)
+  }
+  
+  CombinationsSample
 }
